a5a911c95563f9ab648cf4ece62db4dec0a51f6f
Hugo Jr. <00161417@uca.edu.sv>
2018-06-14 07:20:48 -0600

    Cuando inicia jugador 1, termina bien el trabajo y cuando inicia jugador 2 termina el programa sin dejar que el jugador 1 comience su turno

--- src/hugoriverasworld/HugoRiverasWorld.java
+++ src/hugoriverasworld/HugoRiverasWorld.java
@@ -5,14 +5,6 @@
  */
 package hugoriverasworld;
 
-import abstractfactory.AbstractFactory;
-import abstractfactory.FactoryProducer;
-import abstractfactory.edificaciones.Edificacion;
-import abstractfactory.razas.Raza;
-
-import builder.Milicia;
-import builder.MiliciaBuilder;
-import builder.elementos.Escuadron;
 /**
  *
  * @author HugoJr. <Hugo Rivera at 00161417@uca.edu.sv>
@@ -20,33 +12,13 @@ import builder.elementos.Escuadron;
 public class HugoRiverasWorld {
 
     public static void main(String[] args) {
-        //hacer menu para elegir que crear
-        //AbstractFactory factory;
         
-        //PARA EL MENU, ESTE CODIGO DE ABAJO PUEDE SER LA OPC 1 DE CREAR VEHICULOS AEREOS
-//        factory = FactoryProducer.getFactory("Edificacion");
-//        Edificacion edificacion = factory.getEdificacion("Edificacion constructora vehiculos aereos");
-//        edificacion.construir();
-//        
-//        factory = FactoryProducer.getFactory("Raza");
-//        Raza raza = factory.getRaza("Nazis");
-//        raza.crear();
-        
-        
-        MenuRaza m  = MenuRaza.getInstance();
+        Menu m  = Menu.getInstance();
         m.showMain();
 
 //        MenuMenu mm = MenuMenu.getInstance();
 //        mm.showMain();
       
-        
-        
-//        Escuadron creador = new Escuadron();
-//        MiliciaBuilder miliciabuilder1 = new MiliciaBuilder(creador);
-//        
-//        miliciabuilder1.BuildAMilicia();
-//        
-//        Milicia milicia1 = miliciabuilder1.getMilicia();
     }
     
 }
--- src/hugoriverasworld/Menu.java
+++ src/hugoriverasworld/Menu.java
@@ -19,58 +19,173 @@ import builder.elementos.VehiculoAereo;
 import builder.elementos.VehiculoTerrestre;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 /**
  *
  * @author HugoJr. <Hugo Rivera at 00161417@uca.edu.sv>
  */
-public class MenuMenu {
-    private static MenuMenu instance;
+public class Menu {
+    private static Menu instance;
     
-    private MenuMenu(){
+    private Menu(){
     }
     
-    public static MenuMenu getInstance(){
+    public static Menu getInstance(){
         if (instance == null){
-            synchronized (MenuMenu.class){
+            synchronized (Menu.class){
                 if (instance == null){
-                    instance = new MenuMenu();
+                    instance = new Menu();
                 }
             }
         }
         return instance;
     }
     
-    private void opciones(){
-        String opciones = "1 ) Crear edificacion recolectora de recurso 1\n" + 
-                "2 ) Crear edificacion recolectora de recurso 2\n" + 
-                "3 ) Crear edificacion recolectora de recurso 3\n" +
-                "4 ) Crear edificacion entrenar milicias\n" +
-                "5 ) Crear edificacion construir vehiculos terrestres\n" +
-                "6 ) Crear edificacion construir vehiculos aereos\n" +
-                "7 ) Crear Escuadron\n" +
-                "8 ) Crear Especialista\n" +
-                "9 ) Crear vehiculo terrestre\n" +
-                "10 ) Crear vehiculo aereo\n" +
-                "11 ) Salir\n";
-    }
+    
+//    private void opciones(){
+//        String opciones = "1 ) Nazis\n" + "2 ) Aliados\n" + "3 ) Japos\n" + "4 ) Salir\n";
+//    }
     
     public void showMain(){
-        int opcion = 0;
-        boolean m = false; 
+        String alias;
+        int jugador = 0;
+        int opcion;
+        int opcion1 = 0;
+        boolean m = false;
+        boolean n = false;
         Scanner sn = new Scanner(System.in);
         AbstractFactory factory;
         
-//        try {
-//                opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opcion ", null));
-//            } catch (NumberFormatException e) {
-//                opcion = 12;
-//            }
-
-//MOSTRAR LOS RECURSOS QUE EL JUGADOR POSEE AL INICIO PARA CONSTRUIR, Y PONER EL PRECIO DE CADA EDIFICACION A PARTE DE MOSTRAR LA VIDA
+        random();
+    }
+    
+    /**
+     * Cuando inicia jugador 1, termina bien el trabajo
+     * cuando inicia jugador 2 termina el programa sin dejar que
+     * el jugador 1 comience su turno
+     */
+    private void random(){
+        boolean bande = true;
+        String alias;
+        Scanner sn = new Scanner(System.in);
+        int jugador = (int) (Math.random() * 2) + 1;
+        if(jugador == 1){
+            System.out.println("JUGADOR 1 ==========================");
+            System.out.println("INGRESA TU ALIAS");
+            alias = sn.next();
+            menus();
+            jugador = 2;
+            bande = false;
+        }
+        if(jugador == 2){
+            System.out.println("JUGADOR 2 ==========================");
+            System.out.println("INGRESA TU ALIAS");
+            alias = sn.next();
+            menus();
+            jugador = 1;
+            bande = false;
+        }
+        //bande = false;
+    }
+    
         
-        while(!m){
+        
+        
+        
+        
+        
+        
+//        boolean flag = true;
+//        boolean bande = true;
+//        
+//        
+//        
+//        while(flag){
+//            jugador = (int)(Math.random() * 2) + 1;
+//            if(jugador == 1){
+//                System.out.println("JUGADOR 1 ==========================");
+//                System.out.println("INGRESA TU ALIAS");
+//                alias = sn.next();
+//                //jugador=2;
+//                menus();
+//                //bande = false;
+//            }
+//            else {
+//                System.out.println("JUGADOR 2 ==========================");
+//                System.out.println("INGRESA TU ALIAS");
+//                alias = sn.next();
+//                //jugador=1;
+//                menus();
+//            }
+//            //flag = false;
+//        }
+//        flag = false;
+//        }
+        
+    
+    
+
+        /**
+         * Se ejecuta el primer menu para elegir Raza
+         * y luego se ejecuta el segundo menu
+         * para crear las edificaciones
+         */
+    public void menus(){
+        String alias;
+        int jugador;
+        int opcion;
+        int opcion1 = 0;
+        boolean m = false;
+        boolean n = false;
+        Scanner sn = new Scanner(System.in);
+        AbstractFactory factory;
+        while(!m){  
+            System.out.println("\nELIGE TU RAZA PARA DOMINAR AL MUNDO");
+            System.out.println("\n1 ) Nazis\n" + "2 ) Aliados\n" + "3 ) Japos\n" + "4 ) Salir\n");
+
+            try {
+                System.out.println("Escribe una de las opciones por favor");
+                opcion = sn.nextInt();
+
+                switch(opcion){
+                    case 1:
+                        System.out.println("\n");
+                        factory = FactoryProducer.getFactory("Raza");
+                        Raza razaNazis = factory.getRaza("Nazis");
+                        razaNazis.crear();
+                        m = true;
+                        break;            
+                    case 2:
+                        System.out.println("\n");
+                        factory = FactoryProducer.getFactory("Raza");
+                        Raza razaAliados = factory.getRaza("Aliados");
+                        razaAliados.crear();
+                        m = true;
+                        break;
+                    case 3:
+                        System.out.println("\n");
+                        factory = FactoryProducer.getFactory("Raza");
+                        Raza razaJapos = factory.getRaza("Japos");
+                        razaJapos.crear();
+                        m = true;
+                        break;
+                    case 4:
+                        System.out.println("\n+++++++++++++++++++++++++++++++++++++");
+                        m = true;
+                        break;
+                        //return;
+                    default:
+                        System.err.println("ERROR! Ingresa una opcion valida por favor");
+                }   
+
+            } catch (InputMismatchException e) {
+                System.out.println("Inserta un numero por favor");
+                sn.next();
+            }
+        }
+        while(!n){
+            System.out.println("\nCONSTRUYE TU IMPERIO");
             System.out.println("\n1 ) Crear edificacion recolectora de recurso 1\n" + 
                 "2 ) Crear edificacion recolectora de recurso 2\n" + 
                 "3 ) Crear edificacion recolectora de recurso 3\n" +
@@ -82,47 +197,53 @@ public class MenuMenu {
                 "9 ) Crear vehiculo terrestre\n" +
                 "10 ) Crear vehiculo aereo\n" +
                 "11 ) Salir\n");
-            
+
             try {
                 System.out.println("Escribe una de las opciones por favor");
-                opcion = sn.nextInt();
-                
-                switch(opcion){
+                opcion1 = sn.nextInt();
+
+                switch(opcion1){
                     case 1:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionRecur1 = factory.getEdificacion("Edificacion recolectora de recursos 1");
                         edificacionRecur1.construir();
+                        n = true;
                         break;
                     case 2:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionRecur2 = factory.getEdificacion("Edificacion recolectora de recursos 2");
                         edificacionRecur2.construir();
+                        n = true;
                         break;
                     case 3:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionRecur3 = factory.getEdificacion("Edificacion recolectora de recursos 3");
                         edificacionRecur3.construir();
+                        n = true;
                         break;
                     case 4:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionMilicia = factory.getEdificacion("Edificacion entrenadora de milicia");
                         edificacionMilicia.construir();
+                        n = true;
                         break;
                     case 5:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionTerrestres = factory.getEdificacion("Edificacion constructora vehiculos terrestres");
                         edificacionTerrestres.construir();
+                        n = true;
                         break;
                     case 6:
                         System.out.println("\n");
                         factory = FactoryProducer.getFactory("Edificacion");
                         Edificacion edificacionAereos = factory.getEdificacion("Edificacion constructora vehiculos aereos");
                         edificacionAereos.construir();
+                        n = true;
                         break;
                     case 7:
                         System.out.println("\n");
@@ -130,6 +251,7 @@ public class MenuMenu {
                         MiliciaBuilder miliciabuilder1 = new MiliciaBuilder(creador1);
                         miliciabuilder1.BuildAMilicia();
                         Milicia milicia1 = miliciabuilder1.getMilicia();
+                        n = true;
                         break;
                     case 8:
                         System.out.println("\n");
@@ -137,6 +259,7 @@ public class MenuMenu {
                         MiliciaBuilder miliciabuilder2 = new MiliciaBuilder(creador2);
                         miliciabuilder2.BuildAMilicia();
                         Milicia milicia2 = miliciabuilder2.getMilicia();
+                        n = true;
                         break;
                     case 9:
                         System.out.println("\n");
@@ -144,6 +267,7 @@ public class MenuMenu {
                         VehiculoBuilder vehiculobuilder1 = new VehiculoBuilder(creador3);
                         vehiculobuilder1.BuildAVehiculo();
                         Vehiculo vehiculo1 = vehiculobuilder1.getVehiculo();
+                        n = true;
                         break;
                     case 10:
                         System.out.println("\n");
@@ -151,21 +275,24 @@ public class MenuMenu {
                         VehiculoBuilder vehiculobuilder2 = new VehiculoBuilder(creador4);
                         vehiculobuilder2.BuildAVehiculo();
                         Vehiculo vehiculo2 = vehiculobuilder2.getVehiculo();
+                        n = true;
                         break;
                     case 11:
                         System.out.println("\n");
                         System.out.println("Chaoo....");
-                        m = true;
+                        n = true;
                         break;
                         //return;
                     default:
                         System.err.println("ERROR! Ingresa una opcion valida por favor");
                 }         
-                
+
             } catch (InputMismatchException e) {
                 System.out.println("Inserta un numero por favor");
                 sn.next();
             }
         }
     }
+    
+    //Aqui va el resto del juego
 }
